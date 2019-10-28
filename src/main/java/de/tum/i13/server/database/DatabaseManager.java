6f11a2c0cf3013b8f6dbcddfd511dc017eef49fd
Asela Dasanayaka <aselarbd@gmail.com>
2019-10-28 08:09:52 +0100

    Added File operations for database and Tested

---
+++ src/main/java/de/tum/i13/server/database/DatabaseManager.java
@@ -0,0 +1,154 @@
+package de.tum.i13.server.database;
+
+import java.io.*;
+
+
+public class DatabaseManager {
+
+    private String directoryToStore;
+
+    private File a_d_file ;
+    private File e_h_file ;
+    private File i_n_file ;
+    private File o_s_file ;
+    private File t_z_file ;
+    private File number_file;
+    private File special_file;
+
+    private FileOperations fileOperations;
+
+    DatabaseManager(String directoryToStore){
+        this.directoryToStore = directoryToStore;
+        try {
+            init();
+            fileOperations = new FileOperations();
+        } catch (IOException e) {
+            e.printStackTrace();
+            // Log
+        }
+    }
+
+    public String get (String key){
+        File databaseFile = getDatabaseFile(key);
+        String value = null;
+
+        try {
+            value = fileOperations.getValue(key,databaseFile);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            //log
+        }
+        return value;
+    }
+
+    public int put (String key, String value){
+        File databaseFile = getDatabaseFile(key);
+
+        try {
+            if (null == fileOperations.getValue(key,databaseFile)){
+                if (1 == fileOperations.write(key,value,databaseFile))
+                    return 1;
+
+            }else {
+                if(1 == fileOperations.update(key,value,databaseFile,false))
+                    return 2;
+
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            //log
+        }
+        return -1;
+    }
+
+    public int delete (String key){
+        File databaseFile = getDatabaseFile(key);
+
+        try {
+            if (null != fileOperations.getValue(key,databaseFile)){
+                if(1 == fileOperations.update(key,"",databaseFile,true))
+                    return 1;
+
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            //log
+        }
+        return -1;
+    }
+
+
+
+    private File getDatabaseFile (String key){
+        char control = Character.toLowerCase(key.charAt(0));
+        File databaseFile;
+
+        switch (control){
+            case 'a': case 'b': case 'c': case 'd':
+                databaseFile = a_d_file;
+                break;
+            case 'e': case 'f': case 'g': case 'h':
+                databaseFile = e_h_file;
+                break;
+            case 'i': case 'j': case 'k': case 'l': case 'm': case 'n':
+                databaseFile = i_n_file;
+                break;
+            case 'o': case 'p': case 'q': case 'r': case 's':
+                databaseFile = o_s_file;
+                break;
+            case 't': case 'u': case 'v': case 'w': case 'x': case 'y': case 'z':
+                databaseFile = t_z_file;
+                break;
+            case '1': case '2': case '3': case '4': case '5': case '6': case '7': case '8': case '9': case '0':
+                databaseFile = number_file;
+                break;
+            default:
+                databaseFile = special_file;
+        }
+        return databaseFile;
+    }
+
+
+    public String getDirectoryToStore() {
+        return directoryToStore;
+    }
+
+    public void setDirectoryToStore(String directoryToStore) {
+        this.directoryToStore = directoryToStore;
+    }
+
+    private void init () throws IOException {
+
+        a_d_file = new File(this.directoryToStore+"\\a_d.txt");
+        e_h_file = new File(this.directoryToStore+"\\e_h.txt");
+        i_n_file = new File(this.directoryToStore+"\\i_n.txt");
+        o_s_file = new File(this.directoryToStore+"\\o_s.txt");
+        t_z_file = new File(this.directoryToStore+"\\t_z.txt");
+        number_file = new File(this.directoryToStore+"\\number.txt");
+        special_file = new File(this.directoryToStore+"\\special.txt");
+
+        // TODO : If and Log status
+        if (!a_d_file.createNewFile());
+            //log
+        if(!e_h_file.createNewFile());
+            //log
+        if(!i_n_file.createNewFile());
+        //log
+        if(!o_s_file.createNewFile());
+        //log
+        if(!t_z_file.createNewFile());
+        //log
+        if(!number_file.createNewFile());
+        //log
+        if(!special_file.createNewFile());
+        //log
+    }
+
+
+
+    // TODO Close the connection and end
+    public int close () {
+        return 0;
+    }
+}
---
+++ src/main/java/de/tum/i13/server/database/FileOperations.java
@@ -0,0 +1,88 @@
+package de.tum.i13.server.database;
+
+import java.io.*;
+
+public class FileOperations {
+    /**
+     * write function writes new key value pairs to database files
+     * @param key : key of new tuple
+     * @param value : value of new tuple
+     * @param fileName : filename that tuple should store
+     * @return : returns 1 if process successful
+     * @throws IOException : If errors in file writing operation
+     */
+    public synchronized int write (String key, String value, File fileName) throws IOException {
+
+        BufferedWriter bw = new BufferedWriter( new FileWriter(fileName,true) );
+        bw.write(key+","+value);
+        bw.flush();
+        bw.newLine();
+        bw.close();
+
+        return 1;
+    }
+
+    /**
+     * Update function update and delete a value in the database file
+     * @param key : key need to update
+     * @param value : Value that need to change
+     * @param fileName : File name that update happen
+     * @param delete : if operation is delete pass this param as true otherwise false
+     * @return : return 1 if update successful
+     * @throws IOException : IO exception throws if errors in buffer reader / writer
+     */
+    public synchronized int update (String key, String value, File fileName, Boolean delete) throws IOException {
+
+        BufferedReader br = new BufferedReader( new FileReader(fileName) );
+        File tempFile = new File(fileName.getParent()+"\\"+ fileName.getName()+"_temp.txt");
+        BufferedWriter bw = new BufferedWriter( new FileWriter(tempFile));
+
+        String record;
+        while( ( record = br.readLine() ) != null ) {
+            String [] keyValuePair = record.split(",");
+
+
+            if (keyValuePair[0].equals(key) && !delete){
+                bw.write(key+","+value);
+            }else if(keyValuePair[0].equals(key) && delete){
+              continue;
+            } else {
+                bw.write(record);
+            }
+            bw.flush();
+            bw.newLine();
+        }
+        bw.close();
+        br.close();
+
+        fileName.delete();
+        tempFile.renameTo(fileName);
+
+        return 1;
+    }
+
+    /**
+     * getValue function returns value for corresponding key
+     * @param key: Key of database tuple
+     * @param fileName: File name that database is stored
+     * @return : value of given key in database file. If key is absent returns null
+     * @throws IOException : If files not found or error with buffer reader or reading the file
+     */
+    public synchronized String getValue (String key, File fileName) throws IOException {
+
+        BufferedReader br = new BufferedReader( new FileReader(fileName) );
+        String record;
+        while( ( record = br.readLine() ) != null ) {
+
+            String [] keyValuePair = record.split(",");
+
+            if (keyValuePair[0].equals(key)){
+                br.close();
+                return keyValuePair[1];
+            }
+        }
+        br.close();
+        return null;
+    }
+
+}
