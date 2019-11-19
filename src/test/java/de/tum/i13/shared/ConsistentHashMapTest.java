f8579c7e46db6cb85006e338d5a1d76bcb44b3c2
Nico Thomas <nithom@online.de>
2019-11-19 16:32:19 +0100

    Tests for parsing a keyrange string

--- src/test/java/de/tum/i13/shared/ConsistentHashMapTest.java
+++ src/test/java/de/tum/i13/shared/ConsistentHashMapTest.java
@@ -6,12 +6,17 @@ import java.net.InetSocketAddress;
 import java.security.NoSuchAlgorithmException;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class ConsistentHashMapTest {
     InetSocketAddress ip1 = new InetSocketAddress("192.168.1.1", 80); // be8e4f546de43337d7f0d4637a796478
     InetSocketAddress ip2 = new InetSocketAddress("192.168.1.2", 80); // c8088b91cb0f2fbcbdf107bd31e3d195
     InetSocketAddress ip3 = new InetSocketAddress("192.168.1.3", 80); // 0da0828d3687114976e0edb80e0c54d5
 
+    // keyrange representation of a consistent hash map with the above entries
+    final String keyrangeString = "0da0828d3687114976e0edb80e0c54d5,be8e4f546de43337d7f0d4637a796478,192.168.1.3:80;" +
+            "be8e4f546de43337d7f0d4637a796478,c8088b91cb0f2fbcbdf107bd31e3d195,192.168.1.1:80;" +
+            "c8088b91cb0f2fbcbdf107bd31e3d195,0da0828d3687114976e0edb80e0c54d5,192.168.1.2:80;";
     @Test
     void testConsistentHashMap() throws NoSuchAlgorithmException {
         ConsistentHashMap consistentHashMap = new ConsistentHashMap();
@@ -42,11 +47,37 @@ class ConsistentHashMapTest {
         consistentHashMap.put(ip2);
         consistentHashMap.put(ip3);
 
-        String krString = consistentHashMap.getKeyrangeString();
+        String generatedKrString = consistentHashMap.getKeyrangeString();
 
         // expect keyrange to contain the elements ordered by their start hashes
-        assertEquals("0da0828d3687114976e0edb80e0c54d5,be8e4f546de43337d7f0d4637a796478,192.168.1.3:80;" +
-                "be8e4f546de43337d7f0d4637a796478,c8088b91cb0f2fbcbdf107bd31e3d195,192.168.1.1:80;" +
-                "c8088b91cb0f2fbcbdf107bd31e3d195,0da0828d3687114976e0edb80e0c54d5,192.168.1.2:80;", krString);
+        assertEquals(keyrangeString, generatedKrString);
+    }
+
+    @Test
+    void testFromKeyrangeString() throws NoSuchAlgorithmException {
+        ConsistentHashMap parsedMap = ConsistentHashMap.fromKeyrangeString(keyrangeString);
+
+        // IP1 is the closest to the hash of "key", cf. test case above
+        assertEquals(ip1, parsedMap.get("key"));
+        // test addresses themselves
+        assertEquals(ip1, parsedMap.get("192.168.1.1:80"));
+        assertEquals(ip2, parsedMap.get("192.168.1.2:80"));
+        assertEquals(ip3, parsedMap.get("192.168.1.3:80"));
+    }
+
+    @Test
+    void testFromKeyrangeStringInvalidInput() throws NoSuchAlgorithmException {
+        String[] illegalInputs = new String[] {
+                "",
+                "be8e4f546de43337d7f0d4637a796478,be8e4f546de43337d7f0d4637a796478,192.168.1.1.foo:asdf", // invalid IP
+                "be8e4f546de43337d7f0d4637a796478,be8e4f546de43337d7f0d4637a796478,192.168.1.1:80", // no semicolon
+                "be8e4f546de43337d7f0d4637a796478,be8e4f546de43337d7f0d4637a796478,192.168.1.1:80,foobar;", // additional arg
+                "a,b,192.168.1.1:80", // invalid hashes
+                "be8e4f546de43337d7f0d4637a796478,be8e4f546de43337d7f0d4637a796478,192.168.1.1:80;a,s,127.0.0.1:80,d;", // invalid 2nd item
+                "foobar;be8e4f546de43337d7f0d4637a796478,be8e4f546de43337d7f0d4637a796478,192.168.1.1:80;" // invalid 1st item
+        };
+        for (String input : illegalInputs) {
+            assertThrows(IllegalArgumentException.class, () -> {ConsistentHashMap.fromKeyrangeString(input);});
+        }
     }
 }
