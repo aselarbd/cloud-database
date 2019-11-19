ec91519ffef84de2969c6eb2a76f426cde4cf1fb
Nico Thomas <nithom@online.de>
2019-11-19 12:54:44 +0100

    Add a test for keyrange string generation and create parsing of keyrange strings to new consistent hash map instances

--- src/test/java/de/tum/i13/shared/ConsistentHashMapTest.java
+++ src/test/java/de/tum/i13/shared/ConsistentHashMapTest.java
@@ -8,13 +8,13 @@ import java.security.NoSuchAlgorithmException;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class ConsistentHashMapTest {
+    InetSocketAddress ip1 = new InetSocketAddress("192.168.1.1", 80); // be8e4f546de43337d7f0d4637a796478
+    InetSocketAddress ip2 = new InetSocketAddress("192.168.1.2", 80); // c8088b91cb0f2fbcbdf107bd31e3d195
+    InetSocketAddress ip3 = new InetSocketAddress("192.168.1.3", 80); // 0da0828d3687114976e0edb80e0c54d5
 
     @Test
     void testConsistentHashMap() throws NoSuchAlgorithmException {
         ConsistentHashMap consistentHashMap = new ConsistentHashMap();
-        InetSocketAddress ip1 = new InetSocketAddress("192.168.1.1", 80); // be8e4f546de43337d7f0d4637a796478
-        InetSocketAddress ip2 = new InetSocketAddress("192.168.1.2", 80); // c8088b91cb0f2fbcbdf107bd31e3d195
-        InetSocketAddress ip3 = new InetSocketAddress("192.168.1.3", 80); // 0da0828d3687114976e0edb80e0c54d5
 
         String key = "key"; // 3c6e0b8a9c15224a8228b9a98ca1531d
 
@@ -33,5 +33,20 @@ class ConsistentHashMapTest {
         consistentHashMap.remove(ip1);
 
         assertEquals(ip2, consistentHashMap.get(key));
+    }
+
+    @Test
+    void testGetKeyrangeString() throws NoSuchAlgorithmException {
+        ConsistentHashMap consistentHashMap = new ConsistentHashMap();
+        consistentHashMap.put(ip1);
+        consistentHashMap.put(ip2);
+        consistentHashMap.put(ip3);
+
+        String krString = consistentHashMap.getKeyrangeString();
+
+        // expect keyrange to contain the elements ordered by their start hashes
+        assertEquals("0da0828d3687114976e0edb80e0c54d5,be8e4f546de43337d7f0d4637a796478,192.168.1.3:80;" +
+                "be8e4f546de43337d7f0d4637a796478,c8088b91cb0f2fbcbdf107bd31e3d195,192.168.1.1:80;" +
+                "c8088b91cb0f2fbcbdf107bd31e3d195,0da0828d3687114976e0edb80e0c54d5,192.168.1.2:80;", krString);
     }
 }
