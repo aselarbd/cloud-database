415e075434b1d5a7c878d3ca5abbbbdf53ecb578
Mathis Engelbart <mathis.engelbart@gmail.com>
2020-01-12 22:05:11 +0100

    handle cancelledkey exceptions to unsubscribe lost clients

--- src/main/java/de/tum/i13/server/kv/pubsub/SubscriptionService.java
+++ src/main/java/de/tum/i13/server/kv/pubsub/SubscriptionService.java
@@ -2,11 +2,15 @@ package de.tum.i13.server.kv.pubsub;
 
 import de.tum.i13.kvtp2.Message;
 import de.tum.i13.kvtp2.MessageWriter;
+import de.tum.i13.server.kv.KVServer;
 import de.tum.i13.shared.ConsistentHashMap;
 import de.tum.i13.shared.Constants;
 import de.tum.i13.shared.KVItem;
+import de.tum.i13.shared.Log;
 
+import java.io.IOException;
 import java.net.InetSocketAddress;
+import java.nio.channels.CancelledKeyException;
 import java.util.*;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.ExecutorService;
@@ -14,6 +18,8 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.LinkedBlockingQueue;
 
 public class SubscriptionService {
+
+    public static final Log logger = new Log(SubscriptionService.class);
 
     private Map<InetSocketAddress, MessageWriter> clientWriters = Collections.synchronizedMap(new HashMap<>());
     private Map<InetSocketAddress, Set<String>> clients = Collections.synchronizedMap(new HashMap<>());
@@ -31,16 +37,11 @@ public class SubscriptionService {
     }
 
     public void unsubscribe(String key, InetSocketAddress clientAddress) {
-        Set<String> subscribedKeys = clients.get(clientAddress);
-        Set<InetSocketAddress> clients = subscriptions.get(key);
-
-        if (subscribedKeys != null) {
-            subscribedKeys.remove(key);
+        if (subscriptions.containsKey(key)) {
+            subscriptions.get(key).remove(clientAddress);
         }
-
-        if (clients != null) {
-            clients.remove(clientAddress);
-        }
+        clients.remove(clientAddress);
+        clientWriters.remove(clientAddress);
     }
 
     public void run() {
@@ -55,7 +56,7 @@ public class SubscriptionService {
         });
     }
 
-    private void notifyClient(InetSocketAddress dest, KVItem update) {
+    private void notifyClient(InetSocketAddress dest, KVItem update) throws IOException {
         Message notification = new Message("pubsub_update");
         notification.put("key", update.getKey());
 
@@ -64,6 +65,12 @@ public class SubscriptionService {
         } else {
             notification.put("value", update.getValue());
         }
-        clientWriters.get(dest).write(notification);
+        try {
+            clientWriters.get(dest).write(notification);
+        } catch (CancelledKeyException e) {
+            logger.info("Client has gone away, cancelling subscription");
+            clientWriters.get(dest).close();
+            unsubscribe(update.getKey(), dest);
+        }
     }
 }
